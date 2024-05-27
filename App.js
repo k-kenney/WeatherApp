80fcd3e1d4b0c4d30a45efd16815880728fc9d31
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-05-27 11:18:17 -0600

    implemented API key

--- App.js
+++ App.js
@@ -1,17 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ActivityIndicator, StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
+import * as Location from "expo-location";
+import { WEATHER_API_KEY } from "@env";
+
 
 const App = () => {
-const [loading, setLoading] = useState(true)
-if (loading) {
-  return (
-    <View style={styles.container}>
-      <ActivityIndicator size={'large'} color={'blue'} />
-    </View>
-  )
-}
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [weather, setWeather] = useState([]);
+  const [lat, setLat] = useState([]);
+  const [lon, setLon] = useState([]);
+
+
+
+  if (weather) {
+    console.log(weather);
+  }
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={"large"} color={"blue"} />
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
       <Tabs />
@@ -21,9 +36,9 @@ if (loading) {
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
+    justifyContent: "center",
     flex: 1,
-  }
-})
+  },
+});
 
 export default App;
---
+++ src/hooks/useGetWeather.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from "./src/components/Tabs";
+import * as Location from "expo-location";
+import { WEATHER_API_KEY } from "@env";
+
+const [loading, setLoading] = useState(true);
+const [error, setError] = useState(null);
+const [weather, setWeather] = useState([]);
+const [lat, setLat] = useState([]);
+const [lon, setLon] = useState([]);
+
+export const useGetWeather = () => {
+    const fetchWeatherData = async () => {
+        try {
+          const res = await fetch(
+            `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+          );
+          const data = await response.json();
+          setWeather(data);
+        } catch (e) {
+          setError("Could not fetch weather");
+        } finally {
+          setLoading(false);
+        }
+      };
+    
+      console.log(WEATHER_API_KEY);
+    
+      useEffect(() => {
+        (async () => {
+          let { status } = await Location.requestForegroundPermissionsAsync();
+          if (status !== "granted") {
+            setError("permission to access location was denied");
+            return;
+          }
+          let location = await Location.getCurrentPositionAsync({});
+          setLat(location.coords.latitude);
+          setLon(location.coords.longitude);
+          await fetchWeatherData;
+        })();
+      }, [lat, lon]);
+}
