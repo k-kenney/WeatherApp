edf69f72e534cb16098c37c63bdf0194d9730937
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-05-27 06:31:22 -0600

    added example useState and useEffect hooks. Added loading wheel and state to App

---
+++ src/demonstration/Counter.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const [newCount, setNewCount] = useState(0)
+  useEffect(() => {
+    console.log(`Count changed`)
+    return () => {console.log('useeffect clean up')}
+  }, [count]);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{`count: ${count}`}</Text>
+      <Button
+        color={"red"}
+        title={"Increase the count"}
+        onPress={() => setCount(count + 1)}
+      />
+      <Button
+        color={"group"}
+        title={"Decrease the count"}
+        onPress={() => setCount(count - 1)}
+      />
+      <Button
+        color={"red"}
+        title={"Increase the count"}
+        onPress={() => setNewCount(count + 1)}
+      />
+      <Button
+        color={"group"}
+        title={"Decrease the count"}
+        onPress={() => setNewCount(count - 1)}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "orange",
+  },
+  title: {
+    alignSelf: "center",
+    fontSize: 25,
+    marginTop: 25,
+  },
+});
+export default Counter;
